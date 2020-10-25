ab07a1d80a2fb93d73106b4d0b993c6688564263
김요셉 <yoseb.kim@linecorp.com>
2020-10-25 22:17:02 +0900

    chapter02

--- chapter-02/src/main/java/org/rpis5/chapters/chapter_02/rx_mytest/RxMyTestMain.java
+++ chapter-02/src/main/java/org/rpis5/chapters/chapter_02/rx_mytest/RxMyTestMain.java
@@ -15,22 +15,32 @@ public class RxMyTestMain {
                         //걍 map에 3,2,2,3,1 순서대로 담는거고 이후 아무것도 없으면 걍 onNext에 순차적으로 전달
 //                        .just(3,2,2,3,1);됨
                         //0,1,2가 map에 담김
-                        .range(0, 3)
+                        .range(3, 5)
                         //위 작업 3번 반복
                         .repeat(3)
-                        // 로그찍고 +10해서 리턴
+//                        // 로그찍고 +10해서 리턴
                         .map(k->{
                             log.info(String.valueOf(k+10));
                             return k+10;
                         })
+                        .filter(f->f>15)
                         //이 라인에서 2초 대기
                         //메인 Thread가 끝나면 안돌아서 아래 sleep 줬음
 //                        .delay(2000, TimeUnit.MILLISECONDS)
                         //map 값 하나하나 음수부호로 변경
-                        .map(s -> -s)
+//                        .map(s -> -s)
+                        //위 map으로 하면 비동기라 순서가 바뀜
+                        //아래와 같이 concat으로 하면 순서는 유지되지만 성능은....
+                        .concatMap(c->Observable
+                                .just(c)
+                                .delay(100, TimeUnit.MILLISECONDS)
+                                .map(s -> -s)
+                        )
                         //앞에 2개 값 스킵
                         .skip(2)
-                        //총 3개만 값 받기
+                        //현재 값 갯수를 바로 onNext에 전달하고 onCompleted해버림
+//                        .count()
+//                        //총 3개만 값 받기
                         .take(4);
 
 //        19:29:13.327 [main] INFO org.rpis5.chapters.chapter_02.rx_mytest.RxMyTestMain - 10
